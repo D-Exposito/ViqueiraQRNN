Fixed chain rule claculation of gradient

examples/CESGA_use_cases/ViqueiraQRNN/viqueira_QRNN_circuit.py
@@ -26,12 +26,11 @@ class CircuitQRNNError(Exception):
     pass
 
 
-def add_EMCZ_ansatz(circuit: CunqaCircuit, nE: int, nM: int, x: np.array, theta: np.array, repeat_encode: int, repeat_evolution:int , time_step: int):
+def EMCZ2_encoder(nE: int, nM: int, x: np.array, theta: np.array, repeat_encode: int, repeat_evolution: int, time_step: int) -> CunqaCircuit:
     """
-    Function that adds to a circuit the ansatz for the EMCZ QRNN algorithm, see https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZladder2p1.png for a picture.
+    Creates the encoder block circuit from the EMCZ QRNN algorithm, see https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZladder2p1.png for a picture.
 
     Args:
-        circuit (CunqaCircuit): circuit to which the ansatz will be added. Must have nE + nM qubits.
         nE (int): number of qubits for the Environment/Exchange register
         nM (int): number of qubits for the Memory register
         theta (numpy.array): Trainable parameters for encoding and evolution unitaries. Vector lenght: 2nE*R + 2(nE + nM)*L + nE.
@@ -42,6 +41,8 @@ def add_EMCZ_ansatz(circuit: CunqaCircuit, nE: int, nM: int, x: np.array, theta:
 
     """
     assert np.shape(x)[1] == nE, "Error: The data x should have nE columns." 
+
+    encoder = CunqaCircuit(nE)
 
     group1 = 2*nE*repeat_encode
     group2 = 2*(nE+nM)*repeat_evolution
@@ -65,46 +66,84 @@ def add_EMCZ_ansatz(circuit: CunqaCircuit, nE: int, nM: int, x: np.array, theta:
     # Encoding block
     for i in range(repeat_encode):
         for qubit in range(nE):
-            circuit.ry(x[time_step][qubit], qubit)
-            circuit.rx(orange[2*i*nE + 2*qubit], qubit)
-            circuit.rz(orange[2*i*nE + 2*qubit + 1], qubit)
+            encoder.ry(x[time_step][qubit], qubit)
+            encoder.rx(orange[2*i*nE + 2*qubit], qubit)
+            encoder.rz(orange[2*i*nE + 2*qubit + 1], qubit)
     
     # Non-theta dependent part of the encoding
     for qubit in range(nE):
-        circuit.ry(x[time_step][qubit], qubit)
+        encoder.ry(x[time_step][qubit], qubit)
+        
+def EMCZ2_evolver(nE: int, nM: int, theta: np.array, repeat_encode: int, repeat_evolution: int) -> CunqaCircuit:
+    """
+    Creates the evolution block circuit from the EMCZ QRNN algorithm, see https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZladder2p1.png for a picture.
+
+    Args:
+        nE (int): number of qubits for the Environment/Exchange register
+        nM (int): number of qubits for the Memory register
+        theta (numpy.array): Trainable parameters for encoding and evolution unitaries. Vector lenght: 2nE*R + 2(nE + nM)*L + nE.
+        repeat_encode (int): number of times that the encoding block should be repeated.
+        repeat_evolution (int): number of times that the evolution block should be repeated.
+
+    Returns:
+        evolver (<class cunqa.CunqaCircuit>): (nE + nM)-qubit circuit implementing the evolution block of the EMCZ2 algorithm
+    """
+    encoder = CunqaCircuit(nE)
+
+    group1 = 2*nE*repeat_encode
+    group2 = 2*(nE+nM)*repeat_evolution
+    group3 = 1*nE
+    total_lenght = group1+group2+group3
+
+    # Check correct format of theta
+    if len(theta)==total_lenght:
+        pass
+    else:
+        logger.error(f"Theta must have lenght equal to {total_lenght} but a lenght {len(theta)} vector was provided.")
+        raise  CircuitQRNNError
+    
+    # Slice theta into the parameters that will be used for the encoding part (orange on the image),
+    # the evolution part (blue on the image) and the final evolution part (white on the image).
+    # Image, again: https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZladder2p1.png
+    orange = theta[:group1]
+    blue = theta[group1:group1+group2]
+    white = theta[group1+group2:]
+
+    evolver = CunqaCircuit(nE+nM)
 
     # Evolution block
     for j in range(repeat_evolution):
         for qubit in range(nE+nM-1): # Last qubit missing
 
-            circuit.rx(blue[2*j*(nE+nM-1) + 2*qubit], qubit)
-            circuit.rz(blue[2*j*(nE+nM-1) + 2*qubit + 1], qubit)
-            circuit.cz(qubit, qubit + 1)
+            evolver.rx(blue[2*j*(nE+nM-1) + 2*qubit], qubit)
+            evolver.rz(blue[2*j*(nE+nM-1) + 2*qubit + 1], qubit)
+            evolver.cz(qubit, qubit + 1)
 
         # I separate the last iteration beacause it doesn't have a CZ with the next qubit
-        circuit.rx(blue[(2*j+1)*(nE+nM-1)], nE + nM - 1)
-        circuit.rz(blue[(2*j+1)*(nE+nM-1) + 1], nE + nM - 1)
+        evolver.rx(blue[(2*j+1)*(nE+nM-1)], nE + nM - 1)
+        evolver.rz(blue[(2*j+1)*(nE+nM-1) + 1], nE + nM - 1)
 
     # Final part of the evolution
     for qubit in range(nE):
-        circuit.rx(white[qubit], qubit)
-        
+        evolver.rx(white[qubit], qubit)
 
-def add_EMCZ3_ansatz(circuit: CunqaCircuit, nE: int, nM: int, x: np.array, theta: np.array, repeat_encode: int, repeat_evolution:int , time_step: int):
+    return evolver
+
+def EMCZ3_encoder(nE: int, nM: int, x: np.array, theta: np.array, repeat_encode: int, repeat_evolution:int , time_step: int) -> CunqaCircuit:
     """
-    Function that adds to a circuit the ansatz for the EMCZ3 QRNN algorithm, see https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZme3.png for a picture.
+    Creates the encoder block circuit from the EMCZ3 QRNN algorithm, see https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZladder2p1.png for a picture.
 
     Args:
-        circuit (CunqaCircuit): circuit to which the ansatz will be added. Must have nE + nM qubits.
         nE (int): number of qubits for the Environment/Exchange register
         nM (int): number of qubits for the Memory register
-        theta (numpy.array): Trainable parameters for encoding and evolution unitaries. Vector lenght: 3nE*R + 3(nE + nM)*L + 3nE.
+        theta (numpy.array): Trainable parameters for encoding and evolution unitaries. Vector lenght: 2nE*R + 2(nE + nM)*L + nE.
         x (numpy.array): Input data. Its shape must be (nT, nE), where this is the number of qubits on each register of the EMC QRNN circuit (function below).
         repeat_encode (int): number of times that the encoding block should be repeated.
         repeat_evolution (int): number of times that the evolution block should be repeated.
         time_step (int): indicates in which point of the time series we're on, ie the row of x that should be used.
 
     """
+    encoder = CunqaCircuit(nE)
     assert np.shape(x)[1] == nE, "Error: The data x should have nE columns." 
 
     group1 = 3*nE*repeat_encode
@@ -129,39 +168,79 @@ def add_EMCZ3_ansatz(circuit: CunqaCircuit, nE: int, nM: int, x: np.array, theta
     # Encoding block
     for i in range(repeat_encode):
         for qubit in range(nE):
-            circuit.ry(x[time_step][qubit], qubit)
-            circuit.rx(orange[3*i*nE + 3*qubit], qubit)
-            circuit.rz(orange[3*i*nE + 3*qubit + 1], qubit)
-            circuit.rx(orange[3*i*nE + 3*qubit + 2],qubit)
+            encoder.ry(x[time_step][qubit], qubit)
+            encoder.rx(orange[3*i*nE + 3*qubit], qubit)
+            encoder.rz(orange[3*i*nE + 3*qubit + 1], qubit)
+            encoder.rx(orange[3*i*nE + 3*qubit + 2],qubit)
     
     # Non-theta dependent part of the encoding
     for qubit in range(nE):
-        circuit.ry(x[time_step][qubit], qubit)
+        encoder.ry(x[time_step][qubit], qubit)
+
+    return encoder
+
+def EMCZ3_evolver(nE: int, nM: int, theta: np.array, repeat_encode: int, repeat_evolution:int) -> CunqaCircuit:
+    """
+    Creates the evolution block circuit from the EMCZ QRNN algorithm, see https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZladder2p1.png for a picture.
+
+    Args:
+        nE (int): number of qubits for the Environment/Exchange register
+        nM (int): number of qubits for the Memory register
+        theta (numpy.array): Trainable parameters for encoding and evolution unitaries. Vector lenght: 2nE*R + 2(nE + nM)*L + nE.
+        repeat_encode (int): number of times that the encoding block should be repeated.
+        repeat_evolution (int): number of times that the evolution block should be repeated.
+
+    Returns:
+        evolver (<class cunqa.CunqaCircuit>): (nE + nM)-qubit circuit implementing the evolution block of the EMCZ2 algorithm
+    """
+    evolver = CunqaCircuit(nE+nM)
+    assert np.shape(x)[1] == nE, "Error: The data x should have nE columns." 
+
+    group1 = 3*nE*repeat_encode
+    group2 = 3*(nE+nM)*repeat_evolution
+    group3 = 3*nE
+    total_lenght = group1+group2+group3
+
+    # Check correct format of theta
+    if len(theta)==total_lenght:
+        pass
+    else:
+        logger.error(f"Theta must have lenght equal to {total_lenght} but a lenght {len(theta)} vector was provided.")
+        raise  CircuitQRNNError
+    
+    # Slice theta into the parameters that will be used for the encoding part (orange on the image),
+    # the evolution part (blue on the image) and the final evolution part (white on the image).
+    # Image, again: https://github.com/jdani98/qutims/blob/release/0.2/.images/quantum_ansatz_CZme3.png
+    orange = theta[:group1]
+    blue = theta[group1:group1+group2]
+    white = theta[group1+group2:]
 
     # Evolution block
     for j in range(repeat_evolution):
         # E register part (CZ are performed)
         for qubit in range(nE): 
 
-            circuit.rx(blue[3*j*(nE+nM-1) + 3*qubit], qubit)
-            circuit.rz(blue[3*j*(nE+nM-1) + 3*qubit + 1], qubit)
-            circuit.rx(blue[3*j*(nE+nM-1) + 3*qubit + 2], qubit)
+            evolver.rx(blue[3*j*(nE+nM-1) + 3*qubit], qubit)
+            evolver.rz(blue[3*j*(nE+nM-1) + 3*qubit + 1], qubit)
+            evolver.rx(blue[3*j*(nE+nM-1) + 3*qubit + 2], qubit)
 
             for qubit_m in range(nM):
-                circuit.cz(qubit, nE + qubit_m)
+                evolver.cz(qubit, nE + qubit_m)
 
         # M register part (CZ are received)
         for qubit in range(nE): 
 
-            circuit.rx(blue[3*j*(nE+nM-1) + 3*qubit], qubit)
-            circuit.rz(blue[3*j*(nE+nM-1) + 3*qubit + 1], qubit)
-            circuit.rx(blue[3*j*(nE+nM-1) + 3*qubit + 2], qubit)
+            evolver.rx(blue[3*j*(nE+nM-1) + 3*qubit], qubit)
+            evolver.rz(blue[3*j*(nE+nM-1) + 3*qubit + 1], qubit)
+            evolver.rx(blue[3*j*(nE+nM-1) + 3*qubit + 2], qubit)
 
     # Final part of the evolution
     for qubit in range(nE):
-        circuit.rx(white[3*i*nE + 3*qubit], qubit)
-        circuit.rz(white[3*i*nE + 3*qubit + 1], qubit)
-        circuit.rx(white[3*i*nE + 3*qubit + 2], qubit)
+        evolver.rx(white[3*i*nE + 3*qubit], qubit)
+        evolver.rz(white[3*i*nE + 3*qubit + 1], qubit)
+        evolver.rx(white[3*i*nE + 3*qubit + 2], qubit)
+
+    return evolver   
 
     
     
@@ -212,7 +291,9 @@ class CircuitQRNN:
                 raise CircuitQRNNError
 
             self.circuit.measure([i for i in range(nE)], [time_step*nE + i for i in range(nE)])
-            self.circuit.reset([i for i in range(nE)]) # This instruction needs to be implemented hehe        
+            self.circuit.reset([i for i in range(nE)]) 
+
+        self.circuit.save_state()     
         
     def parameters(self, new_x: np.array, new_theta: np.array) -> list:
         """

examples/CESGA_use_cases/ViqueiraQRNN/viqueira_QRNN_model.py
@@ -21,6 +21,7 @@ from viqueira_QRNN_circuit import CircuitQRNN
 from viqueira_gradients_and_costs import GradientMethod, CostFunction
 from cunqa.logger import logger
 from cunqa.qutils import getQPUs
+from cunqa.qjob import QJob
 
 
 class ViqueiraQRNN:
@@ -47,13 +48,29 @@ class ViqueiraQRNN:
         self.circuit = CircuitQRNN(nE, nM, nT, repeat_encode, repeat_evolution)
 
         self.qpus=getQPUs(local=False) 
-        self.qjobs = list(map(lambda x: self.circuit.run_on_QPU(x,shots=shots), self.qpus)) # Submits the circuit with parameters zero on all QPUs
+        # Submit the circuit with parameters zero on all QPUs
+        self.qjobs = [self.circuit.run_on_QPU(qpu, shots=shots, method="density_matrix") for qpu in self.qpus]
+    
 
-    def calc_observable(self, qjob, x, theta, observ):
-        pass
+    ########################## CIRCUIT CALCULATION METHODS #####################
 
+    def bind_parameters(self, qjob, x, theta) -> QJob:
+        return qjob.upgrade_parameters(self.circuit.parameters(x, theta))
 
-    def train(self, population: list[np.array], y_labels: list[np.array], theta_init: np.array = None, gradient_method: Optional[str] = "finite_differences", cost_func: Optional[str] = "rmse", learn_rate: float = 1e-3, epochs: int = 2000):
+    def calc_observable(self, qjob, observable = None) -> np.array:
+        # TODO: improve this method to a flexible one once we have an observable calculation pipeline
+        probs = qjob.result.probabilities(per_qubit = True, partial = list(range(self.nE)))
+
+        return np.array([prob_qubit[0]-prob_qubit[1] for prob_qubit in probs]) # ad hoc calculation of <Z> observable
+    
+    def evaluate_circ(self, qjob, x, theta) -> np.array:
+        job = self.bind_parameters(qjob, x, theta)
+        obs_result = self.calc_observable(job)
+        return obs_result
+
+    ########################## FIT, PREDICT AND VALIDATE MODEL #########################
+
+    def fit(self, population: list[np.array], y_labels: list[np.array], theta_init: np.array = None, learn_rate: float = 1e-3, epochs: int = 2000, gradient_method: Optional[str] = "finite_differences", cost_func: Optional[str] = "rmse"):
         """
         Method for training the theta parameters of the EMCZ recursive neural network. Uses the gradient method chosen by the user, parallelizing between 
         different QPUs using CUNQA.
@@ -82,13 +99,14 @@ class ViqueiraQRNN:
         with open(logfile, 'a') as f:
 
             for epoch in epochs:
+                # TODO; improve this best_loss initialization as it depends on the time series elements being on [0, 1]
                 best_loss = len(theta_init) * 100 # Unreasonably large number to initialize best_loss and inmediatly update it
                 for i, time_series in enumerate(population):
 
                     gradient = self.calc_gradient(circuit=self.circuit, qjobs=self.qjobs , time_series=time_series, theta_now=theta_aux,  y_true=y_labels[i], cost_func=self.calc_cost)
                     theta_aux += learn_rate * gradient
                     
-                    new_result = self.qjobs[randint(0, len(self.qjobs))].upgrade_parameters(self.circuit.parameters(time_series, theta_aux)).result.probabilities
+                    new_result = self.evaluate_circ(self.qjobs[randint(0, len(self.qjobs))], time_series, theta_aux)          
 
                     loss_i = self.calc_cost(new_result, y_labels[i])
                     if loss_i < best_loss:
@@ -101,9 +119,6 @@ class ViqueiraQRNN:
 
         self._trained = True   
         logger.debug(f"Optimal theta found: {self.theta}.")
-
-        
-    
 
 
     def predict(self, new_time_series: np.array) -> np.array:
@@ -120,7 +135,7 @@ class ViqueiraQRNN:
             logger.error("Model should be trained before trying to make predictions")
             raise SystemExit
         
-        return self.qjobs[randint(0, len(self.qjobs))].upgrade_parameters(self.circuit.parameters(new_time_series, self.theta)).result.probabilities
+        return self.evaluate_circ(self.qjobs[randint(0, len(self.qjobs))], new_time_series, self.theta)
 
 
 
@@ -132,6 +147,8 @@ class ViqueiraQRNN:
             new_time_series (np.array):
             y_new (np.array):
             cost_func (str):
+        Return:
+            (float): 
         """
         if not self._trained:
             logger.error("Model should be trained before trying to evaluate its predictions")

examples/CESGA_use_cases/ViqueiraQRNN/viqueira_gradients_and_costs.py
@@ -58,10 +58,10 @@ class CostFunction:
     
     #################### DERIVATIVES ####################
 
-    def rmse_deriv(self, prediction, y_true):
+    def rmse_deriv(self, prediction, y_true) -> np.array:
         rmse = self.rmse(prediction, y_true)
 
-        return 2*sum([prediction[i]-y_true[i] for i in range(len(y_true))])/(len(y_true)*2*rmse)
+        return 2*np.array([ prediction[i]-y_true[i] for i in range(len(y_true)) ])/(len(y_true)*2*rmse)
     
     #################### INTERFACE METHODS ####################
 
@@ -131,19 +131,21 @@ class GradientMethod:
         """
         n = len(theta_now)
         n_qjobs = len(qjobs)
-        gradient = [0.0 for _ in range(n)]
+        gradient = np.array([0.0 for _ in range(n)])
 
         # We will traverse the components of theta n_qjobs elements at a time
         # on a loop. First we go through the last n % n_qjobs objects and 
         # WE OPTIMIZE BY ADDING THE NON-PERTURBED CIRCUIT ON THIS BATCH (it will be our reference)
         final_start = n-(n % n_qjobs)
         final_results = gather(
-            [self.perturbed_i_circ(qjobs[i], circuit, time_series, theta_now, final_start + i, diff) for i in range(n % n_qjobs)] 
+            [perturbed_i_circ(qjobs[i], circuit, time_series, theta_now, final_start + i, diff) for i in range(n % n_qjobs)] 
             + [qjobs[-1].upgrade_parameters(circuit.parameters(time_series, theta_now))] 
             )
 
-        reference = final_results.pop().probabilities
-        final_deriv = [ (cost_func(reference, y_true) - cost_func(res.probabilities, y_true))/diff for res in final_results]
+        final_observables = [calc_observable(res, circuit.nE) for res in final_results]
+        reference = calc_observable(final_results.pop(), circuit.nE)
+        
+        final_deriv = [np.dot( cost_func.deriv(obs, y_true), (obs - reference)/diff )  for obs in final_observables]
         gradient[final_start:n] = final_deriv
         
         # We go through the components of theta n_qjobs at a time 
@@ -154,11 +156,13 @@ class GradientMethod:
             end = (i+1)*n_qjobs
 
             # Concurrent execution of circuits with small differences on one component
-            results = gather( [self.perturbed_i_circ(qjob, circuit, time_series, theta_now, start + qjobs.index(qjob), diff) for qjob in qjobs] )
-            deriv = [ (cost_func(reference, y_true) - cost_func(res.probabilities, y_true))/diff for res in results]
+            results = gather( [perturbed_i_circ(qjob, circuit, time_series, theta_now, start + qjobs.index(qjob), diff) for qjob in qjobs] )
+            observables = [[calc_observable(res, circuit.nE) for res in results]]
+
+            deriv = [ np.dot( cost_func.deriv(obs, y_true), (obs - reference)/diff ) for obs in observables]
             gradient[start:end] = deriv
 
-        return np.array(gradient)
+        return gradient
 
 
     def parameter_shift_rule(self, circuit: CircuitQRNN, qjobs: list[QJob], time_series: np.array, theta_now: np.array, y_true: np.array, cost_func: CostFunction):
@@ -189,16 +193,25 @@ class GradientMethod:
             end = (i+1)*n_qjobs
 
             # Concurrent execution of circuits with the parameter shifted +-pi/2 on one component
-            results = gather([self.shifted_i_circ(qjob, circuit, time_series, theta_now, start + qjobs.index(qjob)) for qjob in qjobs])
-            deriv = [(cost_func(plus.probabilities, y_true) + cost_func(minus.probabilities, y_true))/2 for plus, minus in zip(results[0::2], results[1::2])]
+            results = gather([shifted_i_circ(qjob, circuit, time_series, theta_now, start + qjobs.index(qjob)) for qjob in qjobs])
+            observables = [calc_observable(res, circuit.nE) for res in results]
+
+            deriv = [
+                np.dot( (cost_func.deriv(plus, y_true) - cost_func.deriv(minus, y_true))/2, (plus - minus)/2 ) 
+                for plus, minus in zip(results[0::2], results[1::2])
+                ] # zip goes through even and odd elements together
 
             gradient[start:end] = deriv
         
         # Last remaining n % n_qjobs components
         final_start = n-(n % n_qjobs)
-        final_results = gather( [self.shifted_i_circ(qjobs[i], circuit, time_series, theta_now, final_start + i) for i in range(n % n_qjobs)] )
+        final_results = gather( [shifted_i_circ(qjobs[i], circuit, time_series, theta_now, final_start + i) for i in range(n % n_qjobs)] )
+        final_observables = [calc_observable(res, circuit.nE) for res in final_results]
 
-        final_deriv = [ (cost_func(plus.probabilities, y_true) + cost_func(minus.probabilities, y_true))/2 for plus, minus in zip(final_results[0::2], final_results[1::2])] # the zip goes through even and odd elements together
+        final_deriv = [ 
+            np.dot( (cost_func.deriv(plus, y_true) - cost_func.deriv(minus, y_true))/2, (plus - minus)/2 ) 
+            for plus, minus in zip(final_results[0::2], final_results[1::2])
+            ] 
         gradient[final_start:n] = final_deriv
 
         return np.array(gradient)
@@ -216,17 +229,25 @@ class GradientMethod:
     def __call__(self, *args, **kwds):
         return self.method(*args, **kwds)
 
-    #################### AUXILIARY METHODS FOR THE GRADIENTS ####################
+#################### AUXILIARY METHODS ####################
 
-    def perturbed_i_circ(qjob: QJob, circuit : CircuitQRNN, time_series: np.array, theta: np.array, index: int, diff: float):
-        theta_aux = theta; theta_aux[index] += diff
-        return qjob.upgrade_parameters(circuit.parameters(time_series, theta_aux))
+def calc_observable(result, nE, observable = None) -> np.array:
+    # TODO: improve this method to a flexible one once we have an observable calculation pipeline
+    probs = result.probabilities(per_qubit = True, partial = list(range(nE)), interface = False)
+
+    return np.array([prob_qubit[0]-prob_qubit[1] for prob_qubit in probs]) # ad hoc calculation of <Z> observable
+
     
-    def shifted_i_circ(qjob: QJob, circuit : CircuitQRNN, time_series: np.array, theta: np.array, index: int, diff: float):
-        theta_plus = theta; theta_plus[index] += np.pi/2
-        theta_minus = theta; theta_minus[index] -= np.pi/2
 
-        qjob_plus = qjob.upgrade_parameters(circuit.parameters(time_series, theta_plus))
-        qjob_minus = qjob.upgrade_parameters(circuit.parameters(time_series, theta_minus))
-        return qjob_plus, qjob_minus
+def perturbed_i_circ(qjob: QJob, circuit : CircuitQRNN, time_series: np.array, theta: np.array, index: int, diff: float):
+    theta_aux = theta; theta_aux[index] += diff
+    return qjob.upgrade_parameters(circuit.parameters(time_series, theta_aux))
+
+def shifted_i_circ(qjob: QJob, circuit : CircuitQRNN, time_series: np.array, theta: np.array, index: int, diff: float):
+    theta_plus = theta; theta_plus[index] += np.pi/2
+    theta_minus = theta; theta_minus[index] -= np.pi/2
+
+    qjob_plus = qjob.upgrade_parameters(circuit.parameters(time_series, theta_plus))
+    qjob_minus = qjob.upgrade_parameters(circuit.parameters(time_series, theta_minus))
+    return qjob_plus, qjob_minus
 