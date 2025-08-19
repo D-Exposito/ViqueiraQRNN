a09fa2826739efb252e59b6102a22b41e48cb1ad
D-Exposito <dexposito@cesga.es>
2025-08-19 11:43:55 +0200

    change abstract parameter identificator from param_name to label

--- examples/CESGA_use_cases/ViqueiraQRNN/viqueira_QRNN_ansatz.py
+++ examples/CESGA_use_cases/ViqueiraQRNN/viqueira_QRNN_ansatz.py
@@ -42,9 +42,9 @@ def parametric_wrapper(cls_inst):
             # Create a wrapper for each parametric method
             def remembering_wrapper(parametric_method):
                 @functools.wraps(parametric_method)
-                def wrapper(*args, param_name: str = "NoName", **kwargs):
+                def wrapper(*args, label: str = "NoName", **kwargs):
 
-                    cls_inst.param_instructions.append(param_name) # save the name of the parameter
+                    cls_inst.param_instructions.append(label) # save the name of the parameter
 
                     # Ensure all necessary parameter keys are provided and if not, put zeros on them
                     new_args = {}
@@ -71,22 +71,22 @@ def parametric_wrapper(cls_inst):
         for instruction in instructions:
 
             if _is_parametric(instruction):
-                if "param_name" in instruction:
+                if "label" in instruction:
                     # Add zero parameters as placeholders to later apply upgrade_parameters
                     if instruction["name"] in  single_param: # one param
-                        cls_inst.param_instructions.append(instruction["param_name"])
+                        cls_inst.param_instructions.append(instruction["label"])
                         if not "param" in instruction:
                             instruction["param"] = 0.
 
                     if instruction["name"] in two_params: # two params: theta, phi
-                        cls_inst.param_instructions + [instruction["param_name"], instruction["param_name"]]
+                        cls_inst.param_instructions + [instruction["label"], instruction["label"]]
                         if not "theta" in instruction:
                             instruction["theta"] = 0.
                         if not "phi" in instruction:
                             instruction["phi"] = 0.
 
                     if instruction["name"] in three_params: # three params: theta, phi, lam
-                        cls_inst.param_instructions + [instruction["param_name"], instruction["param_name"], instruction["param_name"]]
+                        cls_inst.param_instructions + [instruction["label"], instruction["label"], instruction["label"]]
                         if not "theta" in instruction:
                             instruction["theta"] = 0.
                         if not "phi" in instruction:
@@ -95,7 +95,7 @@ def parametric_wrapper(cls_inst):
                             instruction["lam"] = 0.
 
                     if instruction["name"] in four_params: # four params: theta, phi, lam, gamma
-                        cls_inst.param_instructions + [instruction["param_name"], instruction["param_name"], instruction["param_name"], instruction["param_name"]]
+                        cls_inst.param_instructions + [instruction["label"], instruction["label"], instruction["label"], instruction["label"]]
                         if not "theta" in instruction:
                             instruction["theta"] = 0.
                         if not "phi" in instruction:
@@ -105,7 +105,7 @@ def parametric_wrapper(cls_inst):
                         if not "gamma" in instruction:
                             instruction["gamma"] = 0.
 
-                    del instruction["param_name"]
+                    del instruction["label"]
 
                 else: # Record non-abstract parameters to preserve their values when upgrading the abstract ones
                     if instruction["name"] in single_param:
@@ -264,7 +264,7 @@ class AnsatzQRNN:
         Args:
             recompile (bool): lets the function know if the full circuit should be calculated again
             **params (dict): any parameters to be input, using keyword arguments with the format
-                             param_name1 = value1, param_name2 = [value2_a, value2_b]
+                             label1 = value1, label2 = [value2_a, value2_b]
 
         Return:
             (list[float, int]): ordered values to be placed on the parametric gates
@@ -291,25 +291,25 @@ def EMZC2(nE, nM, repeat_encode, repeat_evolution):
 
     # Define encoding
     for qubit in range(nE):
-            emcz2.encoder.ry(qubit, param_name = "x")
-            emcz2.encoder.rx(qubit, param_name = "theta")
-            emcz2.encoder.rz(qubit, param_name = "theta")
+            emcz2.encoder.ry(qubit, label = "x")
+            emcz2.encoder.rx(qubit, label = "theta")
+            emcz2.encoder.rz(qubit, label = "theta")
 
     for qubit in range(nE):
-        emcz2.final_encoding.ry(qubit, param_name = "x")
+        emcz2.final_encoding.ry(qubit, label = "x")
 
     # Define evolution
     for qubit in range(nE+nM-1): # Last qubit missing
 
-        emcz2.evolver.rx(qubit, param_name = "theta")
-        emcz2.evolver.rz(qubit, param_name = "theta")
+        emcz2.evolver.rx(qubit, label = "theta")
+        emcz2.evolver.rz(qubit, label = "theta")
         emcz2.evolver.cz(qubit, qubit + 1)
          
-    emcz2.evolver.rx(nE + nM - 1, param_name = "theta") # I separate the last iteration beacause it doesn't have a CZ with the next qubit
-    emcz2.evolver.rz(nE + nM - 1, param_name = "theta")
+    emcz2.evolver.rx(nE + nM - 1, label = "theta") # I separate the last iteration beacause it doesn't have a CZ with the next qubit
+    emcz2.evolver.rz(nE + nM - 1, label = "theta")
 
     for qubit in range(nE):
-        emcz2.final_evolution.rx(qubit, param_name = "theta")
+        emcz2.final_evolution.rx(qubit, label = "theta")
 
     return emcz2
 
@@ -324,21 +324,21 @@ def EMZC3(nE, nM, repeat_encode, repeat_evolution):
 
     # Define encoding
     for qubit in range(nE):
-            emcz3.encoder.ry(qubit, param_name = "x")
-            emcz3.encoder.rx(qubit, param_name = "theta")
-            emcz3.encoder.rz(qubit, param_name = "theta")
-            emcz3.encoder.rx(qubit, param_name = "theta")
+            emcz3.encoder.ry(qubit, label = "x")
+            emcz3.encoder.rx(qubit, label = "theta")
+            emcz3.encoder.rz(qubit, label = "theta")
+            emcz3.encoder.rx(qubit, label = "theta")
 
     for qubit in range(nE):
-        emcz3.final_encoding.ry(qubit, param_name = "x")
+        emcz3.final_encoding.ry(qubit, label = "x")
 
     # Define evolution
     # E register part (CZ are performed)
     for qubit in range(nE): 
 
-        emcz3.evolver.rx(qubit, param_name = "theta")
-        emcz3.evolver.rz(qubit, param_name = "theta")
-        emcz3.evolver.rx(qubit, param_name = "theta")
+        emcz3.evolver.rx(qubit, label = "theta")
+        emcz3.evolver.rz(qubit, label = "theta")
+        emcz3.evolver.rx(qubit, label = "theta")
 
         for qubit_m in range(nM):
             emcz3.evolver.cz(qubit, nE + qubit_m)
@@ -346,13 +346,13 @@ def EMZC3(nE, nM, repeat_encode, repeat_evolution):
     # M register part (CZ are received)
     for qubit in range(nM): 
 
-        emcz3.evolver.rx(qubit, param_name = "theta")
-        emcz3.evolver.rz(qubit, param_name = "theta")
-        emcz3.evolver.rx(qubit, param_name = "theta")
+        emcz3.evolver.rx(qubit, label = "theta")
+        emcz3.evolver.rz(qubit, label = "theta")
+        emcz3.evolver.rx(qubit, label = "theta")
 
     for qubit in range(nE):
-        emcz3.final_evolution.rx(qubit, param_name = "theta")
-        emcz3.final_evolution.rz(qubit, param_name = "theta")
-        emcz3.final_evolution.rx(qubit, param_name = "theta")
+        emcz3.final_evolution.rx(qubit, label = "theta")
+        emcz3.final_evolution.rz(qubit, label = "theta")
+        emcz3.final_evolution.rx(qubit, label = "theta")
 
     return emcz3
